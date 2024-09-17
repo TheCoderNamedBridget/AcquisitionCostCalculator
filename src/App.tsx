59fa8fa7e238a112511fda3199370fc082432c50
Bridget Naylor <bridgetnaylor2000@gmail.com>
2024-09-17 13:32:16 -0700

    re-organize files

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css';
 import { InputsProvider } from './Inputs/InputsContext';
 import Dscr from './Pages/Dscr';
-import DebtTable from './components/DebtTable';
+import DebtTable from './Pages/DebtTable';
 import Irr from './Pages/Irr';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import ProfitMarginGraph from './Pages/ProfitMarginGraph';
 
 function App() {
   return (
@@ -13,8 +14,9 @@ function App() {
         <nav>
           <ul>
             <li><Link to="/">DSCR</Link></li>
-            <li><Link to="/irr">IRR</Link></li>
-            <li><Link to="/debt-table">Debt Table</Link></li>
+            <li><Link to="/irr">IRR (wip)</Link></li>
+            <li><Link to="/debt-table">Debt Table (wip)</Link></li>
+            <li><Link to="/profit-margin-graph">Profit Margin (wip)</Link></li>
           </ul>
         </nav>
 
@@ -27,6 +29,7 @@ function App() {
             maxIterations={100}
           />} />
           <Route path="/debt-table" element={<DebtTable />} />
+          <Route path="/profit-margin-graph" element={<ProfitMarginGraph />} />
         </Routes>
       </Router>
     </InputsProvider>
